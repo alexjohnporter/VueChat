4fb2bb7ab3cc0068f66c9bdea64b3b261b7b637f
Alex John Porter <alexporter1992@live.co.uk>
2016-05-04 16:56:14 +0100

    Improve user (#1)
    * updated schema
    
    * removing data from model

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,12 +3,13 @@
     <input v-model="text" @keyup.enter="chat(text)" placeholder="Type here!">
     <button @click.prevent="chat(text)">Talk!</button>
 
-    <p v-for="chat in chats">{{chat.text}} - {{chat.user}}</p>
+    <p v-for="chat in chats">{{chat.message}} - {{chat.user.username}}</p>
 </template>
 
 <script>
     import Firebase from 'firebase'
-    var firebase = new Firebase('https://popping-torch-3416.firebaseio.com/chat/');
+
+    var chats = new Firebase('https://popping-torch-3416.firebaseio.com/chat/messages');
 
     export default {
         data() {
@@ -20,15 +21,20 @@
             getters:{
                 user: function (state) {
                     return state.user;
+                },
+                baseUrl: function (state) {
+                    return state.baseUrl;
                 }
             }
         },
         firebase: {
-            'chats': firebase
+            'chats':chats
         },
         methods: {
             chat: function (text) {
-                firebase.push({'user': this.user, 'text': text.trim()});
+                new Firebase(this.baseUrl + '/chat/')
+                        .child('messages')
+                        .push({'user': this.user, 'message': text.trim()});
                 this.text = '';
             }
         }
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Firebase from 'firebase'
+
 Vue.use(Vuex)
 
 const state = {
+    baseUrl: 'https://popping-torch-3416.firebaseio.com/',
     user: null
 };
 
 const mutations = {
     SET_USER(state, user){
+        let newUser = new Firebase(state.baseUrl + '/user').push(user);
+        user['id'] = newUser.key();
         state.user = user;
     }
 };
